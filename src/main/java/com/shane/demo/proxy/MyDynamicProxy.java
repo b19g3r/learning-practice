d81aca0759e71a230a620ecd2981efb83a2760b5
b19g3r <b19g3r@gmail.com>
2021-04-01 17:42:35 +0800

    :tada: Initial commit.

---
+++ src/main/java/com/shane/demo/proxy/MyDynamicProxy.java
@@ -0,0 +1,49 @@
+package com.shane.demo.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ *
+ * @author shane
+ * @link https://qiankunli.github.io/2020/04/09/java_dynamic_proxy.html
+ */
+public class MyDynamicProxy {
+
+    public static void main(String[] args) {
+        HelloImpl hello = new HelloImpl();
+        MyInvocationHandler handler = new MyInvocationHandler(hello);
+        // 构造代码实例，底层实现是 generateProxyClass byte[] 并进行了类加载
+        Hello proxyHello = (Hello) Proxy.newProxyInstance(HelloImpl.class.getClassLoader(), HelloImpl.class.getInterfaces(), handler);
+        // 调用代理方法
+        proxyHello.sayHello();
+    }
+}
+
+interface Hello {
+    void sayHello();
+}
+
+class HelloImpl implements Hello {
+    @Override
+    public void sayHello() {
+        System.out.println("Hello World");
+    }
+}
+
+class MyInvocationHandler implements InvocationHandler {
+    private final Object target;
+
+    public MyInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args)
+            throws Throwable {
+        // 在生产系统中，我们可以轻松扩展类似逻辑进行诊断、限流等。
+        System.out.println("Invoking sayHello");
+        return method.invoke(target, args);
+    }
+}
