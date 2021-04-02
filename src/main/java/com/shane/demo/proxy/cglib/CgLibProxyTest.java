cebb62426419608377b8b205cbae5452917ede82
b19g3r <b19g3r@gmail.com>
2021-04-02 10:12:19 +0800

    jdk and cglib proxy

---
+++ src/main/java/com/shane/demo/proxy/cglib/CgLibProxyTest.java
@@ -0,0 +1,60 @@
+package com.shane.demo.proxy.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CgLibProxyTest {
+
+    public static void main(String[] args) {
+        //目标对象
+        UserDao target = new UserDao();
+        System.out.println(target.getClass());
+        //代理对象
+        UserDao proxy = (UserDao) new ProxyFactory(target).getProxyInstance();
+        System.out.println(proxy.getClass());
+        //执行代理对象方法
+        proxy.save();
+    }
+}
+
+
+class UserDao{
+
+    public void save() {
+        System.out.println("保存数据");
+    }
+}
+
+class ProxyFactory implements MethodInterceptor {
+
+    //维护一个目标对象
+    private Object target;
+
+    public ProxyFactory(Object target) {
+        this.target = target;
+    }
+
+    //为目标对象生成代理对象
+    public Object getProxyInstance() {
+        //工具类
+        Enhancer en = new Enhancer();
+        //设置父类
+        en.setSuperclass(target.getClass());
+        //设置回调函数
+        en.setCallback(this);
+        //创建子类对象代理
+        return en.create();
+    }
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        System.out.println("开启事务");
+        // 执行目标对象的方法
+        Object returnValue = method.invoke(target, args);
+        System.out.println("关闭事务");
+        return null;
+    }
+}
--- src/main/java/com/shane/demo/proxy/jdk/MyDynamicProxy.java
+++ src/main/java/com/shane/demo/proxy/jdk/MyDynamicProxy.java
@@ -1,4 +1,4 @@
-package com.shane.demo.proxy;
+package com.shane.demo.proxy.jdk;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
